f691d0cee7ac2a32d80f59b949f92741b9d0fa44
akashjain0900 <akashjain.0900@gmail.com>
2020-07-18 23:47:01 +0530

    problem_1-2

--- problem_1/abash.sh
+++ problem_1/abash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mv ../problem_1.txt problem_1.txt
-echo "Hi! I'm Akash ' $(whoami)' and today is $(date)" > problem_1.txt
+echo "Hi! I'm $USER ' $(whoami)' and today is $(date)" > problem_1.txt
